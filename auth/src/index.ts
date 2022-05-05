c77c644328f7326ae007cd70395cd5be967f25ae
Rigo Galicia <rigo.galicia@icloud.com>
2022-05-05 11:47:46 -0600

    Error handler implementation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
+
+import mongoose from 'mongoose';
 
 import { signupRouter } from './routes/signup';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { currentUserRouter } from './routes/current-user';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const PORT = 3000;
 
@@ -15,6 +20,23 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(currentUserRouter);
 
-app.listen(PORT, () => {
-  console.log(`Auth service listening on port ${PORT}`);
+app.all('*', () => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-service:27017/auth');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Auth service listening on port ${PORT}`);
+  });
+}
+
+start();
+
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
 const router = express.Router();
 
 router.post('/api/users/signup',
@@ -13,12 +16,14 @@ router.post('/api/users/signup',
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters')
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
+
+    throw new DatabaseConnectionError();
 
     const { email, password } = req.body;
 
